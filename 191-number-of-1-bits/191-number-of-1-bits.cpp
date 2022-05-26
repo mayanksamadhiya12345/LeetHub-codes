8680371b4f627a830da569c818a5b5222dae675b
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-05-26 15:04:27 +0530

    Time: 8 ms (5.79%), Space: 5.7 MB (99.97%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,21 +2,6 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) 
     {
-        int ans=0;
-        
-        // iterate over the 32 bit integer
-        for(int i=0;i<=31;i++)
-        {
-            // start counting 1 from behind
-            // if it gives not zero value it means we can count that as 1
-            // and after taking that 1 just update our number by using (n/2)
-            if(n%2 != 0)
-            {
-                ans++;   
-            }
-            n = n/2;
-        }
-        
-        return ans;
+        return __builtin_popcount(n);
     }
 };
