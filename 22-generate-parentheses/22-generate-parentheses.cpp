2ec44e918817045c06ac7f9c2ac1e2f702d2ef42
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-05 00:01:05 +0530

    Update 22-generate-parentheses.cpp

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     void helper(vector<string>& res, int open, int close, string pair, int n)
     {
+        // if our pair size reach to the double of given number , it means we have reached to our combination
         if(pair.length()==n*2)
         { 
             res.push_back(pair);
             return;
         }
         
+        // if open parenthesis value is under n then add '(' , and increase open by 1
         if(open < n)
         {
             helper(res,open+1,close,pair+"(",n);
         }
         
+        // we can add close parenthesis only and only when, our close is under open
+        // because we have to make well formed parenthesis
         if(close < open)
         {
             helper(res,open,close+1,pair+")",n);
@@ -23,8 +27,8 @@ public:
     
     vector<string> generateParenthesis(int n) 
     {
-        vector<string> res;
-        helper(res,0,0,"",n);
-        return res;
+        vector<string> res;            // it will store final result
+        helper(res,0,0,"",n);          // recursion
+        return res;                    // returning
     }
 };
