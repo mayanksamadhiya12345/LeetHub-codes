a0494794ece481808229778e96822eade03e4af8
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-06 12:18:11 +0530

    Time: 54 ms (69.58%), Space: 14.6 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -16,6 +16,41 @@ public:
         ListNode* dummy1 = headA;  // for headA
         ListNode* dummy2 = headB;  // for headB
         
+        int cnt1 = 0 , cnt2 = 0;   // for counting the length of both lists
+        
+        // counting the length of Linked List 1
+        while(dummy1 != NULL)
+        {
+            cnt1++;
+            dummy1 = dummy1->next;
+        }
+        
+        // counting the length of Linked List 1
+        while(dummy2 != NULL)
+        {
+            cnt2++;
+            dummy2 = dummy2->next;
+        }
+        
+        // aggain point our dummy1 and dummy2 to headA and headB
+        dummy1 = headA;
+        dummy2 = headB;
+        
+        // here we are finding the max & min length diff and according to that we are moving are dummy node to make both dummies equal
+        int diff;
+        if(cnt1 > cnt2)
+        {
+            diff = cnt1-cnt2;
+            while(diff--)                                  // while diff not get 0 move our dummy
+                dummy1 = dummy1->next;
+        }
+        else
+        {
+            diff = cnt2-cnt1;
+            while(diff--)
+                dummy2 = dummy2->next;
+        }
+        
         // till both dummmy node will not collide
         while(dummy1!=dummy2)
         {
@@ -24,20 +59,12 @@ public:
                 dummy1 = dummy1->next;   // iterate the L1
             }
             
-            // logic behind this assign is to fill the differences 
-            else
-            {
-                dummy1 = headB;          // if dummy1 reached to null then assign it to head OF L2
-            }
             
             if(dummy2!=NULL)
             {
                 dummy2 = dummy2->next;   // iterate the L2
             }
-            else
-            {
-                dummy2 = headA;          // if dummy2 reached to null then assign it to head OF L1
-            }
+
         }
         return dummy1;
     }
