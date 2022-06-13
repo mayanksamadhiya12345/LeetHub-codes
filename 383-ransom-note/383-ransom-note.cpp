a7ad4d168523ba452afcaff6b24f337185e077e3
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-13 13:45:02 +0530

    Time: 44 ms (13.84%), Space: 8.9 MB (14.96%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        // declared map for storing the occurance of characters
+        map<char,int> mp;
+        
+        // storing the magazine string characters occurance because it is super-set here
+        for(int i=0;i<magazine.length();i++)
+        {
+            mp[magazine[i]]++;
+        }
+        
+        // now cheking the characters of ransomNote string in mp (super-set)
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            // if value present in mp then minimize it occurance by 1
+            if(mp[ransomNote[i]] != 0)
+            {
+                mp[ransomNote[i]]--;
+            }
+            
+            // if character is not present then return false
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
