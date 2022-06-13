282d01f874bc2923b8d38bd4449bc78222a548f2
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-13 13:33:57 +0530

    Update 387-first-unique-character-in-a-string.cpp

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int firstUniqChar(string s) 
     {
+        // declared map for storing the occurance of character
         map<char,int> mp;
         
+        // storing occurance of characters
         for(int i=0;i<s.size();i++)
         {
             mp[s[i]]++;
         }
         
+        // check first non repeating character
         for(int i=0;i<s.size();i++)
         {
-            if(mp[s[i]] == 1)
+            if(mp[s[i]] == 1)          // if its count 1 then return it
                 return i;
         }
+        
+        // if there is no unique char then return -1
         return -1;
     }
 };
