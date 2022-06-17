109b1fa6d230d0ed1ac92e3e78f44093d8625e1b
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-17 23:27:13 +0530

    Time: 2 ms (48.40%), Space: 6.3 MB (52.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,35 +3,43 @@ public:
     
     int uniquePaths(int m, int n) 
     {
-        // delare a 2D vector with value -1 initially
-        vector<vector<int>> dp(m,vector<int> (n));
+        // delare a vector that will store prev row values
+        vector<int> prev(n);
         
         // start with base cases
-        dp[0][0] = 1;
+        prev[0] = 1;
         
         // iteration
         for(int i=0;i<m;i++)
         {
+            // it will store the curr computing row value
+            vector<int> curr(n);
             for(int j=0;j<n;j++)
             {
                 // base case
                 if(i==0 && j==0)
-                    dp[0][0] = 1;
+                    curr[j] = 1;
                 else
                 {
+                    // move up so we need prev row , so use it
                     int up = 0;
                     if(i>0)
-                        up = dp[i-1][j];
+                        up = prev[j];
                     
+                    // move left so we need curr row , so use it
                     int left = 0;
                     if(j>0)
-                        left = dp[i][j-1];
+                        left = curr[j-1];
                     
-                    dp[i][j] = up + left;
+                    // store into curr
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        
+        // return the last prev
+        return prev[n-1];
     }
   
 
