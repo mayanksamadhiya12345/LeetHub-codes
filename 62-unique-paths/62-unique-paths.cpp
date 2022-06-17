14bdbea44262d10407908f74f9c081c2906d7a51
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-17 23:20:41 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.36%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int find(int i,int j,vector<vector<int>>& dp)
-    {
-        // edge cases
-        if(i==0 && j==0)
-            return 1;
-        
-        if(i<0 or j<0)
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        // I can move in to directions
-        int up = find(i-1,j,dp);
-        int left = find(i,j-1,dp);
-        
-        return dp[i][j] = up+left;
-    }
+    
     int uniquePaths(int m, int n) 
     {
         // delare a 2D vector with value -1 initially
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return find(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int> (n));
+        
+        // start with base cases
+        dp[0][0] = 1;
+        
+        // iteration
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                // base case
+                if(i==0 && j==0)
+                    dp[0][0] = 1;
+                else
+                {
+                    int up = 0;
+                    if(i>0)
+                        up = dp[i-1][j];
+                    
+                    int left = 0;
+                    if(j>0)
+                        left = dp[i][j-1];
+                    
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
   
 
