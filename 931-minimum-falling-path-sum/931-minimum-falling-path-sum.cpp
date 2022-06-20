2af43c0d0bd353d3c762baea0bfa33936aa52c1c
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-20 23:29:41 +0530

    Time: 15 ms (85.88%), Space: 9.8 MB (94.58%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -5,42 +5,43 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
         int n = matrix.size();
-        vector<vector<int>> dp(n,vector<int> (n,0));
+        vector<int> prev(n,0) , curr(n,0);
         
         // base cases
         for(int j=0;j<n;j++)
         {
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
         
         for(int i=1;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
                 
                 int up_left = matrix[i][j];
                 if(j-1>=0)
-                    up_left += dp[i-1][j-1];
+                    up_left += prev[j-1];
                 else
                     up_left += 1e9;
                 
                 
                 int up_right = matrix[i][j];
                 if(j+1<n)
-                    up_right += dp[i-1][j+1];
+                    up_right += prev[j+1];
                 else
                     up_right += 1e9;
                 
-                dp[i][j] = min(up,min(up_left,up_right));
+                curr[j] = min(up,min(up_left,up_right));
             }
+            prev = curr;
         }
         
         
         int mn = INT_MAX;
         for(int j=0;j<n;j++)
         {
-            mn = min(mn,dp[n-1][j]);
+            mn = min(mn,prev[j]);
         }
         
         return mn;
