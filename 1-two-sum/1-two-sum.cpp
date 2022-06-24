bc875652e3f663a3dcdff33242cee816ddac9d50
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-24 23:30:32 +0530

    Time: 17 ms (68.34%), Space: 11.1 MB (24.60%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        unordered_map<int,int> m;
-        int n=nums.size();
+        vector<pair<int,int>> vt;
         
-        // itearte over the given nums
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            int x=nums[i];               // first number
-            int y=target-x;              // deriving the second number for cheking that it is available or not
-            
-            // if it is present it means we have our target so just return the current index i and second number index that is m[y]
-            if(m.find(y)!=m.end())
-            {
-                return {i,m[y]};
-            }
-            
-            // every time store current nums value with respect to their index value
-            m[nums[i]]=i;
+            vt.push_back({nums[i],i});
+        }
+        sort(vt.begin(),vt.end());
+        int i=0;
+        int j= vt.size()-1;
+        
+        while(i<j)
+        {
+            int sum = vt[i].first + vt[j].first;
+            if(sum==target)
+                return {vt[i].second,vt[j].second};
+            else if(sum > target)
+                j--;
+            else
+                i++;
         }
         
-        // if we don't have any pair of target then return {-1,-1}
         return {-1,-1};
     }
 };
