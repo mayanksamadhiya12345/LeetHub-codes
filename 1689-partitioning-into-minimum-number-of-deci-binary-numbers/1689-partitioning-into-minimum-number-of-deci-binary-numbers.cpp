781541f3bc402e13d8685f8b59764d7b40f0ea89
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-06-27 12:31:56 +0530

    Time: 32 ms (84.21%), Space: 13.5 MB (65.18%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPartitions(string n) 
+    {
+        int len = n.length();
+        int max_digit_in_string = 0;
+        
+        for(auto element : n)
+        {
+            // convert the string number into integer number by subtracting the ascii value.
+            int digit = element - '0';
+            max_digit_in_string = max(max_digit_in_string,digit);
+        }
+        
+        return max_digit_in_string;
+    }
+};
