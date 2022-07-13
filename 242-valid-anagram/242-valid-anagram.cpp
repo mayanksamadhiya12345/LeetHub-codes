0b25e1a191ba2481cf6c35227512150b50844ff8
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-07-13 13:41:06 +0530

    Time: 16 ms (48.89%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,26 +5,9 @@ public:
         if(s.length() != t.length())
             return false;
         
-        // declared map for storing the occurance of characters
-        map<char,int> mp;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
-        // storing the s string characters occurance
-        for(int i=0;i<s.length();i++)
-        {
-            mp[s[i]]++;
-        }
-        
-        // now cheking the characters of t string in mp
-        for(int i=0;i<t.length();i++)
-        {
-            // if character is not present then return false
-            if(mp[t[i]] == 0)
-                return false;
-            
-            // if value present in mp then minimize it occurance by 1
-            else
-                mp[t[i]]--;
-        }
-        return true;
+        return s==t;
     }
 };
