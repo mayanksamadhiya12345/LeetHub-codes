ef12505964cc0264deeb28ce7e8f5bd2613c5454
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-07-19 09:30:42 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows)
     {
-        vector<vector<int>> r(numRows);
+        vector<vector<int>> ans(numRows);
         
         for(int i=0;i<numRows;i++)
         {
-            // resizing the empty vector with i+1 size because in every row column equal to row 
-            r[i].resize(i+1);
+            ans[i].resize(i+1);
             
-            // initializing first and last element 1
-            r[i][0]=r[i][i]=1;
+            // mark first and last element as 1 for current row
+            ans[i][0] = ans[i][i] = 1;
             
             for(int j=1;j<i;j++)
             {
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
             }
         }
-        return r;
+        return ans;
     }
 };
