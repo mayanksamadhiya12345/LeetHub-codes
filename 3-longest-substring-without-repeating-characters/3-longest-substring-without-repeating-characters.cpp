9b778e0f6c553223cc45b8dcf858676f4719023d
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-07-26 16:14:46 +0530

    Time: 7 ms (92.47%), Space: 8.2 MB (81.64%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) 
     {
-        // initially all the chars are unvisited (-1)
         vector<int> mp(256,-1);
         
-        int len=0;
-        int left=0;
-        int right =0;
-        int n=s.size();
+        int l=0;
+        int r=0;
+        int length=0;
+        int n=s.length();
         
-        // till right pointer will not cross boundary
-        while(right<n)
+        while(r<n)
         {
-            // if char already present in our map it means its repeatiting character 
-            // so for avoiding this point our left to just next of curr char
-            if(mp[s[right]]!=-1)
-            {
-                // if element is found in mp then move the left cursor by index+1
-                left=max(mp[s[right]]+1,left);
-            }
+            if(mp[s[r]] != -1)
+                l = max(l,mp[s[r]]+1);
             
-            mp[s[right]]=right;                  // storing the corresposding index for s[right] instaed of -1
-            len=max(len,right-left+1);           // calculating the max length
-            right++;
+            mp[s[r]] = r;
+            length = max(length,r-l+1);
+            r++;
         }
-        return len;
+        return length;
     }
 };
