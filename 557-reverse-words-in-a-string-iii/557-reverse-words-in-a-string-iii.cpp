d7a333ea4bfe443f032dd1b27239be7312048c48
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-07-26 16:08:05 +0530

    Time: 103 ms (8.64%), Space: 269.2 MB (6.51%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -9,22 +9,7 @@ public:
         {
             if(s[i]==' ')
             {
-                int l = 0;                       // left pointer
-                int r = tmp.size()-1;                     // right pointer
-
-                // till left and right pointer do not cross to each other
-                while(l<=r)
-                {
-                    // swapping the left and right values
-                    char temp = tmp[r];
-                    tmp[r] = tmp[l];
-                    tmp[l] = temp;
-
-                    // moving the Pointers
-                    l++;
-                    r--;
-                }
-                
+                reverse(tmp.begin(),tmp.end());
                 if(ans=="")
                 {
                     ans = tmp;
@@ -43,30 +28,11 @@ public:
         }
         if(tmp!=" ")
         {
-            int l = 0;                       // left pointer
-            int r = tmp.size()-1;                     // right pointer
-
-            // till left and right pointer do not cross to each other
-            while(l<=r)
-            {
-                // swapping the left and right values
-                char temp = tmp[r];
-                tmp[r] = tmp[l];
-                tmp[l] = temp;
-
-                // moving the Pointers
-                l++;
-                r--;
-            }
-
+            reverse(tmp.begin(),tmp.end());
             if(ans=="")
-            {
                 ans = tmp;
-            }
             else
-            {
                 ans = ans+" "+tmp;
-            } 
         }
             
         return ans;
