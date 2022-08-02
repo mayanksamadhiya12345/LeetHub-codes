0fe2c06c97870e163be9ee10a2ceccd466acdfb7
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-02 12:32:13 +0530

    Time: 48 ms (19.62%), Space: 17 MB (52.12%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,21 +2,37 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
+//         int n=nums.size();
+//         unordered_map<int,int> mp(n);
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(mp[nums[i]]==1)
+//             {
+//                 return nums[i];
+//             }
+//         }
+//         return 0;
+        
         int n=nums.size();
-        unordered_map<int,int> mp(n);
-        
-        for(int i=0;i<n;i++)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++)
         {
-            mp[nums[i]]++;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            if(mp[nums[i]]==1)
+            if(nums[i]==nums[i+1])
+            {
+                i++;
+                continue;
+            }      
+            else
             {
                 return nums[i];
             }
         }
-        return 0;
+        return nums[n-1];
     }
 };
