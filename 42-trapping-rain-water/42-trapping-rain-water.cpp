01d6ad86cccffd52973100a67df66aff6ea2c7b4
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-02 14:07:36 +0530

    Time: 18 ms (82.58%), Space: 19.7 MB (96.83%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,37 +3,33 @@ public:
     int trap(vector<int>& arr) 
     {
         int n=arr.size();
-        vector<int> leftMax(n);
-        vector<int> rightMax(n);
-
-        int mx=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            if(mx<arr[i])
-            {
-                mx=arr[i];
-                leftMax[i]=mx;
-            }
-            leftMax[i]=mx;
-        }
-
-        mx=INT_MIN;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(mx<arr[i])
-            {
-                mx=arr[i];
-                rightMax[i]=mx;
-            }
-            rightMax[i]=mx;
-        }
-
+        int l=0;
+        int r=n-1;
+        int leftMax=0;
+        int rightMax=0;
         int res=0;
-        for(int i=0;i<n;i++)
-        {
-            res = res + min(leftMax[i],rightMax[i])-arr[i];
-        }
 
+        while(l<=r)
+        {
+            if(arr[l]<=arr[r])
+            {
+                if(leftMax<=arr[l])
+                    leftMax=arr[l];
+                else
+                    res = res + (leftMax-arr[l]);
+
+                l++;
+            }
+            else
+            {
+                if(rightMax<=arr[r])
+                    rightMax=arr[r];
+                else
+                    res = res + (rightMax-arr[r]);
+
+                r--;
+            }
+        }
         return res;
     }
 };
