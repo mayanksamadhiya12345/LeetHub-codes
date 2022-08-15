a3d67227cda7e724ae24584af96555685a4b1009
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-15 11:33:00 +0530

    Time: 15 ms (86.06%), Space: 6.9 MB (95.93%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -8,7 +8,12 @@ public:
         int n = word2.size() ;
         
         // dp declaration
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0) );
+        // vector<vector<int>> dp(m+1, vector<int>(n+1,0) );
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1,0);
+        
+        for(int j=0;j<=n;j++)
+            prev[j]=0;
         
         // iterate over the first words
         for(int i=1;i<=m;i++)
@@ -20,14 +25,15 @@ public:
                 // here we check by using by [m-1][n-1] becuse strings are 0 base indexed that's why we go untill <=m or <=n
                 // if both string characters are same then this will be the part of our LCS so add 1 and check for next one
                 if( word1[i-1] == word2[j-1] )
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 
                 // if not both are same then try to move pointer with respect to other and take maxone
                 else 
-                    dp[i][j] = max( dp[i-1][j], dp[i][j-1] );
+                    curr[j] = max( prev[j], curr[j-1] );
             }
+            prev = curr;
         }
     
-        return m+n-(2*dp[m][n]);
+        return m+n-(2*prev[n]);
     }
 };
