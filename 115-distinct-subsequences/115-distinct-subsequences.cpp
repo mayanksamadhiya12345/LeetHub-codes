f238d6505d2cd9eb97b14ef30ca8ec7eb03dbdeb
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-17 11:06:51 +0530

    Time: 36 ms (85.23%), Space: 6.5 MB (83.30%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -4,23 +4,25 @@ public:
     {
         int n = s.length();
         int m = t.length();
-        vector<vector<double>> dp(n+1,vector<double> (m+1,0));
+        
+        vector<double> prev(m+1,0);
+        vector<double> curr(m+1,0);
         
         // base case
-        // for every j=0 make i 1
-        for(int i=0;i<=n;i++) dp[i][0] = 1;
+        prev[0] = curr[0] = 1;
         
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
                 if(s[i-1]==t[j-1]) 
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];              // i can match it and move j and i can also look for the nest match without moving j
+                    curr[j] = prev[j-1]+prev[j];              // i can match it and move j and i can also look for the nest match without moving j
                 else 
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
             }
+            prev = curr;
         }
     
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
