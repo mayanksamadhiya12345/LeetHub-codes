bf742fc61906c5a60dfd210322df3a9764b67d8b
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-18 16:26:18 +0530

    Time: 15 ms (72.98%), Space: 8.9 MB (67.22%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int find(int i,int j,string &s,string &t,vector<vector<int>>& dp)
-    {
-        if(i<0) return j+1;                // if first string exhausted then I need to perform remanining second string char insertions
-        if(j<0) return i+1;                // if second string exhausted then I need to perform remanining first string char deletions
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        // if they both char are matched then i will do nothin just move my pointers
-        if(s[i]==t[j]) return dp[i][j] = find(i-1,j-1,s,t,dp);
-        
-        // if they don't match so i have three options insert(i,j-1),delete(i-1,j),replace(i-1,j-1) and take min among them
-        return dp[i][j] = 1 + min(find(i,j-1,s,t,dp),min(find(i-1,j,s,t,dp),find(i-1,j-1,s,t,dp)));
-    }
-    
     int minDistance(string s, string t) 
     {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
         
-        return find(n-1,m-1,s,t,dp);
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        
+        return dp[n][m];
     }
 };
