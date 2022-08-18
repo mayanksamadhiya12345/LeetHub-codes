e33a8fa1aed14de69532d9c8542f62fec5577482
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-18 16:30:26 +0530

    Time: 17 ms (65.57%), Space: 6.3 MB (98.09%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,20 +4,23 @@ public:
     {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
         
-        for(int i=0;i<=n;i++) dp[i][0]=i;
-        for(int j=0;j<=m;j++) dp[0][j]=j;
+        // i don't need column , i just need row
+        for(int j=0;j<=m;j++) prev[j]=j;
         
         for(int i=1;i<=n;i++)
         {
+            curr[0]=i;
             for(int j=1;j<=m;j++)
             {
-                if(s[i-1]==t[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
-                else dp[i][j] = 1 + min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                if(s[i-1]==t[j-1]) curr[j] = 0 + prev[j-1];
+                else curr[j] = 1 + min(curr[j-1],min(prev[j],prev[j-1]));
             }
+            prev=curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
