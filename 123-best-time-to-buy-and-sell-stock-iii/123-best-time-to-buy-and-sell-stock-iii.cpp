fc079f953725d174ca884325261b9de6831ac32a
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-25 11:10:16 +0530

    Time: 329 ms (56.77%), Space: 75.4 MB (78.66%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,10 @@
 class Solution {
-public:
-    int find(int idx,int buy,int cap,vector<int>& prices,int n,vector<vector<vector<int>>>& dp)
-    {
-        // base case, either idx cross the boundary or cap reached to 0 we can not make any further profit
-        if(idx==n or cap==0) return 0;
-        
-        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
-        
-        int profit=0;
-        // can buy
-        if(buy==1)
-        {
-                        // buy                                   // not buy
-            return dp[idx][buy][cap] = max(-prices[idx]+find(idx+1,0,cap,prices,n,dp),0+find(idx+1,1,cap,prices,n,dp));
-        }
-        else
-        {
-                      // sell                                    // not sell
-            return dp[idx][buy][cap] = max(prices[idx]+find(idx+1,1,cap-1,prices,n,dp),0+find(idx+1,0,cap,prices,n,dp));
-        }
-    }
-    
+public:    
     int maxProfit(vector<int>& prices) 
     {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> ahead(2,vector<int> (3,0));
+        vector<vector<int>> curr(2,vector<int> (3,0));
         
         // base cases, assign 0 
         // when cap==0 and idx and buy can anything
@@ -37,22 +17,22 @@ public:
             {
                 for(int cap=1;cap<=2;cap++)
                 {
-                    int profit=0;
                     // can buy
                     if(buy==1)
                     {
                                     // buy                                   // not buy
-                        dp[idx][buy][cap] = max(-prices[idx]+dp[idx+1][0][cap],0+dp[idx+1][1][cap]);
+                        curr[buy][cap] = max(-prices[idx]+ahead[0][cap],0+ahead[1][cap]);
                     }
                     else
                     {
                                   // sell                                    // not sell
-                        dp[idx][buy][cap] = max(prices[idx]+dp[idx+1][1][cap-1],0+dp[idx+1][0][cap]);
+                        curr[buy][cap] = max(prices[idx]+ahead[1][cap-1],0+ahead[0][cap]);
                     }
                 }
             }
+            ahead = curr;
         }
         
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 };
