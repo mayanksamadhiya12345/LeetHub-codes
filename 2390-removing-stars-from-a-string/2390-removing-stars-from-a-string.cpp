62e49470f09ebe9a52f43cc46d1cfd0ed4e6d456
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-28 09:53:44 +0530

    Time: 98 ms (33.33%), Space: 25.9 MB (16.67%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans = "";
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i]!= '*')
+                ans += s[i];
+            else
+                ans.pop_back();
+        }
+        
+        return ans;
+    }
+};
