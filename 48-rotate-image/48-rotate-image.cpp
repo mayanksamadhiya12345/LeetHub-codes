d07b346aafd9f3a764b7c1f57bf33c0295876b8c
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-08-30 12:58:05 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,29 +4,18 @@ public:
     {
         int row=matrix.size();
         int column=matrix[0].size();
-        // Transpose
+        
         for(int i=0;i<row;i++)
         {
-            for(int j=0;j<column;j++)
+            for(int j=0;j<i;j++)
             {
-                if(i<j)
-                {
-                    swap(matrix[i][j],matrix[j][i]);
-                }            
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         
-        // Reverse 
         for(int i=0;i<row;i++)
         {
-            int st_col=0;
-            int en_col=column-1;
-            while(st_col<en_col)
-            {
-                swap(matrix[i][st_col],matrix[i][en_col]);
-                st_col++;
-                en_col--;
-            }
-        }     
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
