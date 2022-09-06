f1b0a5466f3c84c73d9a7713f4ca5d205e0db067
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-09-06 10:30:50 +0530

    Time: 78 ms (20.26%), Space: 12.3 MB (82.22%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) 
     {
-        int n=grid.size();
-        if(n==0)
-        {
-            return 0;
-        }
-        int m=grid[0].size();
+        int n=grid.size();           // rows
+        int m=grid[0].size();        // col
+        
+        // if grid is empty it means no island is there
+        if(n==0) return 0;
+        
         int res=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)                         // iterate through the all cells
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1')
+                if(grid[i][j]=='1')                  // if we get cell as value as 1 it means it is the starting of new islands so do res+1 for each new island
                 {
-                    bfs(grid,i,j,n,m);
+                    dfs(grid,i,j,n,m);
                     res+=1;
                 }
             }
         }
         return res;
     }
-    void bfs(vector<vector<char>> &matrix,int x,int y,int r,int c)
+    void dfs(vector<vector<char>> &matrix,int x,int y,int r,int c)
     {
         //making boundaries 
         if(x<0 or y<0 or x>=r or y>=c or matrix[x][y]!='1')
@@ -31,12 +31,12 @@ public:
         }
         
         //mark as visited
-        matrix[x][y]='2';
+        matrix[x][y]='2';                               // make currnt cell as 2 for get rid from from overlaping 
         
         //cheking all four directions
-        bfs(matrix,x-1,y,r,c);      //top
-        bfs(matrix,x+1,y,r,c);      //down
-        bfs(matrix,x,y-1,r,c);      //left
-        bfs(matrix,x,y+1,r,c);      //right
+        dfs(matrix,x-1,y,r,c);      //top
+        dfs(matrix,x+1,y,r,c);      //down
+        dfs(matrix,x,y-1,r,c);      //left
+        dfs(matrix,x,y+1,r,c);      //right
     }
 };
