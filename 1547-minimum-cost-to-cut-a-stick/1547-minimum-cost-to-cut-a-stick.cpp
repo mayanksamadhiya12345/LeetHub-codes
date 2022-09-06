02aa37d74b836d2f6694fcad0ea3f647e82e77c4
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-09-06 15:33:11 +0530

    Time: 117 ms (50.79%), Space: 10.4 MB (16.40%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int find(int i,int j,vector<int>& cuts,vector<vector<int>>& dp)
-    {
-        if(i>j) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int mn=1e9;
-        for(int idx=i;idx<=j;idx++)
-        {
-            // curr stick length (cuts[j+1]-cuts[i-1]) and after this calculate the left part and right part also
-            int cost = (cuts[j+1]-cuts[i-1]) + find(i,idx-1,cuts,dp) + find(idx+1,j,cuts,dp);
-            mn = min(mn,cost);
-        }
-        return dp[i][j] = mn;
-    }
-    
     int minCost(int n, vector<int>& cuts) 
     {
         int c = cuts.size();
@@ -23,8 +7,25 @@ public:
         cuts.insert(cuts.begin(),0);                 // push 0 at the beggining
         sort(cuts.begin(),cuts.end());               // make sure to sort the arrau because after sorting we can easily run a loop to partition the array
         
-        vector<vector<int>> dp(c+1,vector<int> (c+1,-1));
+        vector<vector<int>> dp(c+2,vector<int> (c+2,0));
         
-        return find(1,c,cuts,dp);
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j) continue;          // base case (if there is no stick no need to cut it further)
+                
+                int mn = INT_MAX;
+                for(int idx=i;idx<=j;idx++)
+                {
+                    // curr stick length (cuts[j+1]-cuts[i-1]) and after this calculate the left part and right part also
+                    int cost = (cuts[j+1]-cuts[i-1]) + dp[i][idx-1] + dp[idx+1][j];
+                    mn = min(mn,cost);
+                }
+                dp[i][j] = mn;
+            }
+        }
+        
+        return dp[1][c];
     }
 };
