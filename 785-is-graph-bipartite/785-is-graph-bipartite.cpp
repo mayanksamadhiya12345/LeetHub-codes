8d5ab81a9f0184930aef4a8265dfcb8a0887b7ef
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-09-06 10:30:12 +0530

    Time: 59 ms (16.77%), Space: 13.8 MB (29.55%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     bool check(int node,vector<int>& color,vector<vector<int>>& graph)
     {
-        if(color[node]==-1) color[node]=0;
+        queue<int> q;
+        q.push(node);
+        color[node] = 0;
         
-        for(auto it : graph[node])
+        while(!q.empty())
         {
-            if(color[it]==-1)
+            int nod = q.front();
+            q.pop();
+            
+            for(auto it : graph[nod])
             {
-                color[it] = 1-color[node];
-                if(!check(it,color,graph)) return false;
-            }
-            else
-            {
-                if(color[it]==color[node]) return false;
+                if(color[it]==-1)
+                {
+                    color[it] = 1-color[nod];
+                    q.push(it);
+                }
+                else
+                {
+                    if(color[it]==color[nod]) return false;
+                }
             }
         }
         return true;
@@ -34,4 +42,4 @@ public:
         return true;
     }  
 };
-// using DFS
+// using BFS
