50a7608e852e5388bc9770068f80b4ea326e08d2
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-09-10 11:04:26 +0530

    Time: 46 ms (18.05%), Space: 13.1 MB (27.76%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
+    int find(int idx,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp)
+    {
+        if(idx==prices.size() or cap==0) return 0;
+        
+        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
+        
+        int profit=0;
+        if(buy)
+        {
+            profit = max(-prices[idx]+find(idx+1,0,cap,prices,dp),0+find(idx+1,1,cap,prices,dp));
+        }
+        else
+        {
+            profit = max(prices[idx]+find(idx+1,1,cap-1,prices,dp),0+find(idx+1,0,cap,prices,dp));
+        }
+        return dp[idx][buy][cap] = profit;
+    }
+    
     int maxProfit(int k, vector<int>& prices) 
     {
         int n = prices.size();
-        vector<vector<int>> ahead(2,vector<int> (k+1,0));
-        vector<vector<int>> curr(2,vector<int> (k+1,0));
-        
-        if(k==0 or n<=1)
-            return 0;
-        
-        // base cases, assign 0 
-        // when cap==0 and idx and buy can anything
-        // when idx==n and cap and buy can anything
-        // there is no need to thse write cases because we already define our dp as 0
-        
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int cap=1;cap<=k;cap++)
-                {
-                    // can buy
-                    if(buy==1)
-                    {
-                                    // buy                                   // not buy
-                        curr[buy][cap] = max(-prices[idx]+ahead[0][cap],0+ahead[1][cap]);
-                    }
-                    else
-                    {
-                                  // sell                                    // not sell
-                        curr[buy][cap] = max(prices[idx]+ahead[1][cap-1],0+ahead[0][cap]);
-                    }
-                }
-            }
-            ahead = curr;
-        }
-        
-        return ahead[1][k];
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (k+1,-1)));
+        return find(0,1,k,prices,dp);
     }
 };
